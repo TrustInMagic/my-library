6195b2ebfa48858e27599fe8a7400247465e359b
Tim Ionescu <timionescu@gmail.com>
2023-02-07 22:51:18 +0200

    Add popup, no styles

--- js/script.js
+++ js/script.js
@@ -13,11 +13,12 @@ function Book(title, author, pages, language, read) {
 }
 
 const addBooks = document.querySelector('.add-book button');
-const addNewBook = document.createElement('div');
-addNewBook.classList.add('add-new-book');
+const popUpContainer = document.createElement('div');
+popUpContainer.classList.add('pop-up-container');
+const header = document.querySelector('.header');
 
 addBooks.addEventListener('click', () => {
-  document.body.style = "filter: blur(3px)";
-  
+  header.style = "filter: blur(3px)";
+  document.body.insertBefore(popUpContainer, header);
 })
 
